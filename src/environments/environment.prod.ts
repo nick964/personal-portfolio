c2858965aa49eb2cee93f21310778a9185c60b21
Nick Robinson <nickr964@gmail.com>
2020-11-15 19:58:22 -0500

    adding favicons and fixing footer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCfmUxIbKE0Xm45uAFDbbO038F6625iW_A",
+    authDomain: "nick-personal-f685d.firebaseapp.com",
+    databaseURL: "https://nick-personal-f685d.firebaseio.com",
+    projectId: "nick-personal-f685d",
+    storageBucket: "nick-personal-f685d.appspot.com",
+    messagingSenderId: "780616312531",
+    appId: "1:780616312531:web:ff94bf47a68e53def59626",
+    measurementId: "G-JN0B7KXFMJ"
+  }
 };
